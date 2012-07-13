e978037458e695181f02e49496f937097682d6bd
Tyler Hunt <tyler@tylerhunt.com>
2012-07-13 09:48:22 -0700

    Add specs for Resource#get.

--- spec/brewery_db/resource_spec.rb
+++ spec/brewery_db/resource_spec.rb
@@ -4,6 +4,41 @@ describe BreweryDB::Resource do
   let(:client) { BreweryDB::Client.new }
   let(:resource) { Class.new { include BreweryDB::Resource }.new(client) }
 
+  context '#get' do
+    let(:connection) { stub }
+    let(:response) { stub }
+
+    before do
+      client.configure { |config| config.api_key = 'secret' }
+      connection.stub(:get) { response }
+      resource.stub(:connection) { connection }
+
+      response.stub(:success?) { false }
+      response.stub(:body).and_return('body')
+      response.body.stub(:data) { 'data' }
+    end
+
+    it 'delegates to the connection' do
+      connection.should_receive(:get).with('path', key: 'secret') { response }
+      resource.send(:get, 'path')
+    end
+
+    it 'merges in any additional parameters' do
+      connection.should_receive(:get).with('path', key: 'secret', p: 1) { response }
+      resource.send(:get, 'path', p: 1)
+    end
+
+    it 'returns the response body data if the request is successful' do
+      response.stub(:success?) { true }
+      resource.send(:get, 'path').should == 'data'
+    end
+
+    it 'returns the response body if the request is not successful' do
+      response.stub(:success?) { false }
+      resource.send(:get, 'path').should == 'body'
+    end
+  end
+
   context '#connection' do
     subject { resource.send(:connection) }
 
