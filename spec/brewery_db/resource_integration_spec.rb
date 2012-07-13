58fa47990c9bcd1870e5cee3fe16c87873d39ac8
Tyler Hunt <tyler@tylerhunt.com>
2012-07-13 10:49:40 -0700

    Use concrete methods so we don't need #send.

--- spec/brewery_db/resource_integration_spec.rb
+++ spec/brewery_db/resource_integration_spec.rb
@@ -4,23 +4,39 @@ require 'spec_helper'
 
 describe BreweryDB::Resource, :resource do
   context '#get', vcr: cassette_options do
-    let(:resource) { Class.new { include BreweryDB::Resource }.new(client) }
+    let(:resource) do
+      Class.new {
+        include BreweryDB::Resource
+
+        def ok
+          get('breweries', name: 'Rogue Ales')
+        end
+
+        def bad_request
+          get('breweries')
+        end
+
+        def not_found
+          get('brewery/NOT_FOUND')
+        end
+      }.new(client)
+    end
 
     context 'an OK request' do
-      subject { resource.send(:get, 'breweries', name: 'Rogue Ales').first }
+      subject { resource.ok.first }
 
       its(:name) { should == 'Rogue Ales' }
     end
 
     context 'a bad request' do
-      subject { resource.send(:get, 'breweries') }
+      subject { resource.bad_request }
 
       its(:errorMessage) { should match /data.*invalid/ }
       its(:status) { should eq 'failure' }
     end
 
     context 'a not found request' do
-      subject { resource.send(:get, 'brewery/NOT_FOUND') }
+      subject { resource.not_found }
 
       its(:errorMessage) { should match /not\s+found/ }
       its(:status) { should eq 'failure' }
