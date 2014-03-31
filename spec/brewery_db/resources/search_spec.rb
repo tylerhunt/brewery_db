58d5e9e420551224ed184e106619548669abe42c
Steven Harman <steven@harmanly.com>
2014-03-31 16:23:22 -0400

    Make consistent use of single-quotes

--- spec/brewery_db/resources/search_spec.rb
+++ spec/brewery_db/resources/search_spec.rb
@@ -28,13 +28,13 @@ describe BreweryDB::Resources::Search, :resource do
     end
   end
 
-  context "#upc", :vcr do
+  context '#upc', :vcr do
     subject(:search) { described_class.new(config) }
 
-    context "for an existing upc code" do
+    context 'for an existing upc code' do
       let(:upc) { '030613000043' }
 
-      it "returns a collection with the matched beer" do
+      it 'returns a collection with the matched beer' do
         expect(search.upc(code: upc).map(&:id)).to eq(['LVjHK1'])
       end
     end
